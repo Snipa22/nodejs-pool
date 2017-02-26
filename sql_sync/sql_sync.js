9cc9e0634423e912431fa551df1fce1b65995cd5
Alexander Blair <alex@snipanet.com>
2017-02-26 10:52:26 -0800

    Whoops!

--- sql_sync/sql_sync.js
+++ sql_sync/sql_sync.js
@@ -14,14 +14,16 @@ global.schema = JSON.parse(sql_schema);
 
 let loopCount = 0;
 let updatedCount = 0;
-async.eachSeries(global.schema, function(entry){
+async.eachSeries(global.schema, function(entry, callback){
     global.mysql.query("SELECT * FROM config WHERE module = ? AND item = ?", [entry.module, entry.item]).then(function(rows){
         loopCount += 1;
         if (rows.length > 0){
-            return;
+            return callback();
         }
         updatedCount += 1;
-        global.mysql.query("INSERT INTO config (module, item, item_value, item_type, Item_desc) VALUES (?, ?, ?, ?, ?)", [entry.module, entry.item, entry.item_value, entry.item_type, entry.Item_desc]);
+        global.mysql.query("INSERT INTO config (module, item, item_value, item_type, Item_desc) VALUES (?, ?, ?, ?, ?)", [entry.module, entry.item, entry.item_value, entry.item_type, entry.Item_desc]).then(function(){
+            return callback();
+        });
     });
 }, function(){
     console.log("Updated SQL schema with "+updatedCount+" new rows!  Exiting!");
