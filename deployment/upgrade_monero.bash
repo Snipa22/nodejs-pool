8e81ad2f25e091fcf411a251c85f3311bff6ad7d
Snipa22 <alex@snipanet.com>
2017-04-07 15:42:35 -0700

    Update upgrade_monero.bash

--- deployment/upgrade_monero.bash
+++ deployment/upgrade_monero.bash
@@ -4,7 +4,9 @@ sleep 15
 echo "Continuing install, this will prompt you for your password if you didn't enable passwordless sudo.  Please do not run me as root!"
 cd /usr/local/src/monero
 sudo git checkout .
+sudo git pull
 sudo git checkout v0.10.3.1
+sudo git pull
 curl -L https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v
 sudo rm -rf build
 sudo make -j$(nproc)
