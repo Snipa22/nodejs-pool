ac33f403dc325db2ac1d2f967695c2851b8fd511
Snipa22 <alex@snipanet.com>
2017-10-02 10:18:41 -0700

    Update deploy.bash
    Pointing to Snipa22's backup of the caddy daemon.

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -60,7 +60,7 @@ cd build
 sudo ln -s `pwd` /var/www
 CADDY_DOWNLOAD_DIR=$(mktemp -d)
 cd $CADDY_DOWNLOAD_DIR
-curl -sL "https://caddyserver.com/download/linux/amd64?plugins=http.cors" | tar -xz caddy init/linux-systemd/caddy.service
+curl -sL "https://snipanet.com/caddy.tar.gz" | tar -xz caddy init/linux-systemd/caddy.service
 sudo mv caddy /usr/local/bin
 sudo chown root:root /usr/local/bin/caddy
 sudo chmod 755 /usr/local/bin/caddy
