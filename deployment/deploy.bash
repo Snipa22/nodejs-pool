8a78e37fa6ec76df53082609fa59746acb665395
Vitor Lamas Gatti <vitorgatti@yahoo.com.br>
2017-12-31 22:56:32 -0200

    Update deploy.bash
    the & in line 87 should put the command to the background, but instead hangs the deploy script and the next commands doesn't execute

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -84,7 +84,7 @@ sudo env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin `pwd`/.nvm/versions/node
 cd ~/nodejs-pool
 sudo chown -R $CURUSER. ~/.pm2
 echo "Installing pm2-logrotate in the background!"
-pm2 install pm2-logrotate &
+pm2 install pm2-logrotate
 mysql -u root --password=$ROOT_SQL_PASS < deployment/base.sql
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'authKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'Auth key sent with all Websocket frames for validation.')"
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'secKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'HMAC key for Passwords.  JWT Secret Key.  Changing this will invalidate all current logins.')"
