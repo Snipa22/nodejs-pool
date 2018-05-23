43786993462643772b14546367c55068a0d02533
MoneroOcean <support@moneroocean.stream>
2018-05-23 22:30:36 +0200

    Added &&

--- deployment/upgrade_monero.bash
+++ deployment/upgrade_monero.bash
@@ -2,14 +2,14 @@
 echo "This assumes that you have a standard nodejs-pool install, and will patch and update it to the latest stable builds of Monero."
 sleep 15
 echo "Continuing install, this will prompt you for your password if you didn't enable passwordless sudo.  Please do not run me as root!"
-cd /usr/local/src/monero
-sudo git checkout .
-sudo git checkout master
-sudo git pull
-sudo git checkout v0.12.1.0
-curl -L https://raw.githubusercontent.com/MoneroOcean/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v
-sudo git submodule init
-sudo git submodule update
-sudo rm -rf build
-sudo nice make
+cd /usr/local/src/monero &&\
+sudo git checkout .  &&\
+sudo git checkout master &&\
+sudo git pull &&\
+sudo git checkout v0.12.1.0 &&\
+curl -L https://raw.githubusercontent.com/MoneroOcean/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v &&\
+sudo git submodule init &&\
+sudo git submodule update &&\
+sudo rm -rf build &&\
+sudo nice make &&\
 echo "Done building the new Monero daemon!  Please go ahead and reboot monero with: sudo systemctl restart monero as soon as the pool source is updated!"
