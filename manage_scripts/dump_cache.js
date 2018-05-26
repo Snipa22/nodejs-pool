6ebb8d739ab3dfde83bba1cea2f0712d86632402
MoneroOcean <support@moneroocean.stream>
2018-05-26 16:42:35 +0200

    Removed compatibility layer (ater executing upgrade_cache.js)

--- manage_scripts/dump_cache.js
+++ manage_scripts/dump_cache.js
@@ -5,7 +5,6 @@ const user = argv.user ? argv.user : null;
 
 require("../init_mini.js").init(function() {
 	let txn = global.database.env.beginTxn({readOnly: true});
-	let x = 0;
 	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
 	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
         	cursor.getCurrentString(function(key, data){  // jshint ignore:line
