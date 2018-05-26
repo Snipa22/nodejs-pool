2fdcd614d7236179d3d1be53ccc133d8bb4f654f
MoneroOcean <support@moneroocean.stream>
2018-05-26 14:35:46 +0200

    Changed string output

--- manage_scripts/dump_cache.js
+++ manage_scripts/dump_cache.js
@@ -5,9 +5,8 @@ require("../init_mini.js").init(function() {
 	let x = 0;
 	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
 	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
-        	cursor.getCurrentBinary(function(key, data){  // jshint ignore:line
-			const data2 = JSON.parse(data);
-			console.log(key + ": " + JSON.stringify(data2));
+        	cursor.getCurrentString(function(key, data){  // jshint ignore:line
+			console.log(key + ": " + data);
 			if (++ x > 10) {
 				cursor.close();
 			        txn.commit();
