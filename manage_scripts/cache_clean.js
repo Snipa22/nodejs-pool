35138c21da2ebd80d924f9807d41af1679af6742
MoneroOcean <support@moneroocean.stream>
2018-05-26 19:44:31 +0200

    Fixed scripts

--- manage_scripts/cache_clean.js
+++ manage_scripts/cache_clean.js
@@ -10,12 +10,12 @@ require("../init_mini.js").init(function() {
 	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
 	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
         	cursor.getCurrentString(function(key, data){  // jshint ignore:line
+			if (user && !key.includes(user)) return;
 			if (key.length < 95) { // min XMR address length
 				console.log("Skipping " + key + " key");
 				return;
 			}
 			if (key.includes("identifiers:") || !key.includes("_")) return;
-			if (user && !key.includes(user)) return;
 
 			//let txn2 = global.database.env.beginTxn();
 			if (key.includes("history:") || key.includes("stats:")) {
@@ -48,8 +48,41 @@ require("../init_mini.js").init(function() {
 		        //txn2.commit();
 		});
 	}
+
+	console.log("Deleted items: " + count);
+
+	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
+        	cursor.getCurrentString(function(key, data){  // jshint ignore:line
+			if (user && !key.includes(user)) return;
+			if (key.length < 95) { // min XMR address length
+				console.log("Skipping " + key + " key");
+				return;
+			}
+			if (!key.includes("identifiers:")) return;
+
+			let parts = key.split(/:(.+)/);
+			let key2 = parts[1];
+
+                        try {
+	                	let data2 = JSON.parse(data);
+				let isAlive = false;
+				for (let i in data2) {
+					let stats = global.database.getCache("stats:" + key2 + "_" + data2[i]);
+					if (stats && Date.now() - stats.lastHash > 24*60*60*1000) isAlive = true;
+				}
+				if (!isAlive) {
+					data2 = [];
+					console.log(key + ": found dead key");
+					//let txn2 = global.database.env.beginTxn();
+					//txn2.putString(global.database.cacheDB, key, JSON.stringify(data2));
+					//txn2.commit();
+				}
+			} catch (e) {
+				console.error("Bad cache data with " + key + " key");
+			}
+		});
+	}
 	cursor.close();
         txn.commit();
-	console.log("Deleted items: " + count);
 	process.exit(0);
 });
