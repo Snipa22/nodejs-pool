363a36c1590d66072e2e25f6ca29d40edd70cf05
MoneroOcean <support@moneroocean.stream>
2018-05-26 14:40:43 +0200

    Changed string output

--- manage_scripts/dump_cache.js
+++ manage_scripts/dump_cache.js
@@ -1,17 +1,15 @@
 "use strict";
 
+const argv = require('minimist')(process.argv.slice(2));
+const user = argv.user ? argv.user : null;
+
 require("../init_mini.js").init(function() {
 	let txn = global.database.env.beginTxn({readOnly: true});
 	let x = 0;
 	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
 	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
         	cursor.getCurrentString(function(key, data){  // jshint ignore:line
-			console.log(key + ": " + data);
-			if (++ x > 10) {
-				cursor.close();
-			        txn.commit();
-				process.exit(0);
-			}
+			i (!user || key.includes(user)) console.log(key + ": " + data);
 		});
 	}
 	cursor.close();
