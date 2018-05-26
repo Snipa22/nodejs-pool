476c8b2463712de4cac6e272888f9e37dac8fc00
MoneroOcean <support@moneroocean.stream>
2018-05-26 14:21:54 +0200

    New scripts

---
+++ manage_scripts/dump_cache.js
@@ -0,0 +1,20 @@
+"use strict";
+
+require("../init_mini.js").init(function() {
+	let txn = global.database.env.beginTxn({readOnly: true});
+	let x = 0;
+	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
+	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
+        	cursor.getCurrentBinary(function(key, data){  // jshint ignore:line
+			console.log(key + ": " + data);
+			if (++ x > 10) {
+				cursor.close();
+			        txn.commit();
+				process.exit(0);
+			}
+		});
+	}
+	cursor.close();
+        txn.commit();
+	process.exit(0);
+});
