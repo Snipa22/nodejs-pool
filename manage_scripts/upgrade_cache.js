a3d52a48cc9f93d8f2b2d4f73cb055ba61db135f
MoneroOcean <support@moneroocean.stream>
2018-05-26 16:18:18 +0200

    Added new script

--- manage_scripts/upgrade_cache.js
+++ manage_scripts/upgrade_cache.js
@@ -4,22 +4,23 @@ const argv = require('minimist')(process.argv.slice(2));
 const user = argv.user ? argv.user : null;
 
 require("../init_mini.js").init(function() {
-	let txn = global.database.env.beginTxn();
+	let txn = global.database.env.beginTxn({readOnly: true});
 	let cursor = new global.database.lmdb.Cursor(txn, global.database.cacheDB);
 	for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
         	cursor.getCurrentString(function(key, data){  // jshint ignore:line
 			if (key.includes("history:") || key.includes("stats:") || key.includes("identifiers:")) return;
 			if (!user || key.includes(user)) {
+				let txn2 = global.database.env.beginTxn();
 				if (key.includes("_identifiers")) {
 					let parts = key.split("_");
 					let key2 = parts[0];
 					if (global.database.getCache("identifiers:" + key2)) {
 						console.log(key2 + ": removing outdated _identifiers");
-						txn.del(global.database.cacheDB, key);
+						txn2.del(global.database.cacheDB, key);
 					} else {
 						console.log(key2 + ": moving _identifiers to identifiers:");
-						txn.putString(global.database.cacheDB, "identifiers:" + key2, data);
-						txn.del(global.database.cacheDB, key);
+						txn2.putString(global.database.cacheDB, "identifiers:" + key2, data);
+						txn2.del(global.database.cacheDB, key);
 					}
 				} else {
                                 	try {
@@ -29,39 +30,38 @@ require("../init_mini.js").init(function() {
 								console.log(key + ": removing outdated stats");
 								delete data2["hash"];
 								delete data2["lastHash"];
-								txn.putString(global.database.cacheDB, key, JSON.stringify(data2));
+								txn2.putString(global.database.cacheDB, key, JSON.stringify(data2));
 							} else {
 								console.log(key + ": moving old stats to stats:");
 								let data3 = { hash: data2.hash, lastHash: data2.lastHash };
 								delete data2["hash"];
 								delete data2["lastHash"];
-								txn.putString(global.database.cacheDB, key, JSON.stringify(data2));
-								txn.putString(global.database.cacheDB, "stats:" + key, JSON.stringify(data3));
+								txn2.putString(global.database.cacheDB, key, JSON.stringify(data2));
+								txn2.putString(global.database.cacheDB, "stats:" + key, JSON.stringify(data3));
 							}
 						}
 						if ("hashHistory" in data2) {
 							if (global.database.getCache("history:" + key)) {
 								console.log(key + ": removing outdated history");
 								delete data2["hashHistory"];
-								txn.putString(global.database.cacheDB, key, JSON.stringify(data2));
+								txn2.putString(global.database.cacheDB, key, JSON.stringify(data2));
 							} else {
 								console.log(key + ": moving old history to history:");
 								let data3 = { hashHistory: data2.hashHistory };
 								delete data2["hashHistory"];
-								txn.putString(global.database.cacheDB, key, JSON.stringify(data2));
-								txn.putString(global.database.cacheDB, "history:" + key, JSON.stringify(data3));
+								txn2.putString(global.database.cacheDB, key, JSON.stringify(data2));
+								txn2.putString(global.database.cacheDB, "history:" + key, JSON.stringify(data3));
 							}
 						}
 					} catch (e) {
 						console.error("Bad cache data with " + key + " key");
 					}
 				}
+			        txn2.commit();
 			}
 		});
 	}
 	cursor.close();
-	console.log("Starting write to LMDB");
         txn.commit();
-	console.log("Finishing write to LMDB");
 	process.exit(0);
 });
