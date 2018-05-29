efe2a70172563f4d9cd94372e99b870b3531f985
MoneroOcean <support@moneroocean.stream>
2018-05-29 15:56:10 +0200

    Removed not used files

--- sample_config.sql
+++
@@ -1,6 +0,0 @@
-UPDATE pool.config SET item_value = '' WHERE module = 'pool' and item = 'address';
-UPDATE pool.config SET item_value = '' WHERE module = 'payout' and item = 'feeAddress';
-UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunKey';
-UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunURL';
-UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'emailFrom';
-UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'shareHost';
