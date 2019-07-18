200b7fc055c58ef14d86528baece0de45e918529
MoneroOcean <support@moneroocean.stream>
2019-07-18 12:21:02 -0700

    Updated daemon version

--- deployment/upgrade_monero.bash
+++ deployment/upgrade_monero.bash
@@ -6,7 +6,7 @@ cd /usr/local/src/monero &&\
 sudo git checkout .  &&\
 sudo git checkout master &&\
 sudo git pull &&\
-sudo git checkout v0.14.1.0 &&\
+sudo git checkout v0.14.1.2 &&\
 sudo git submodule init &&\
 sudo git submodule update &&\
 sudo rm -rf build &&\
