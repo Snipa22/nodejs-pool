8a5348c1ff774058a445b593c0f8dec7619ce295
MoneroOcean <support@moneroocean.stream>
2019-08-20 12:27:34 -0700

    Added new script

---
+++ manage_scripts/get_block_hash.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const argv = require('minimist')(process.argv.slice(2));
+
+if (!argv.port) {
+	console.error("Please specify port");
+	process.exit(1);
+}
+const port = argv.port;
+
+if (!argv.hash) {
+	console.error("Please specify height");
+	process.exit(1);
+}
+const height = argv.hash;
+
+require("../init_mini.js").init(function() {
+  global.coinFuncs.getPortAnyBlockHeaderByHash(port, hash, function (err_header, body_header) {
+    console.log("err:"  + JSON.stringify(err_header));
+    console.log("body:" + JSON.stringify(body_header));
+    process.exit(0);
+  });
+});
