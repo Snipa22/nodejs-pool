9cf0dd187a7bcac115c990b80e7612903a25583d
MoneroOcean <support@moneroocean.stream>
2020-01-08 18:29:08 -0800

    Updated LMDB tools

--- deployment/install_lmdb_tools.sh
+++ deployment/install_lmdb_tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~
-git clone https://github.com/LMDB/lmdb
-cd lmdb
-git checkout 4d2154397afd90ca519bfa102b2aad515159bd50
-cd libraries/liblmdb/
+git clone https://github.com/Venemo/node-lmdb.git
+cd node-lmdb
+git checkout v0.7.0
+cd dependencies/lmdb/libraries/liblmdb
 make -j `nproc`
 mkdir ~/.bin
 echo ' ' >> ~/.bashrc
