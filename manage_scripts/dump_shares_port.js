6832017b101be3dfc41f662eb9901496f8171d8a
MoneroOcean <support@moneroocean.stream>
2020-03-19 09:51:53 -0700

    Dump ports

---
+++ manage_scripts/dump_shares_port.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let range = require('range');
+const argv = require('minimist')(process.argv.slice(2));
+
+if (!argv.port) {
+        console.error("Please specify port to dump");
+        process.exit(1);
+}
+const port = argv.port;
+
+let depth = 10;
+if (argv.depth) depth = argv.depth;
+
+console.log("Dumping shares for " + user + " user");
+if (paymentid) console.log("Dumping shares for " + paymentid + " paymentid");
+if (worker)    console.log("Dumping shares for " + worker + " worker");
+
+require("../init_mini.js").init(function() {
+
+        global.coinFuncs.getLastBlockHeader(function (err, body) {
+                if (err !== null) {
+                        console.error("Invalid block header");
+                        process.exit(1);
+                }
+                let lastBlock = body.height + 1;
+                let txn = global.database.env.beginTxn({readOnly: true});
+
+                let cursor = new global.database.lmdb.Cursor(txn, global.database.shareDB);
+                range.range(lastBlock, lastBlock - depth, -1).forEach(function (blockID) {
+                        for (let found = (cursor.goToRange(parseInt(blockID)) === blockID); found; found = cursor.goToNextDup()) {
+                                cursor.getCurrentBinary(function(key, data){  // jshint ignore:line
+                                        let shareData = global.protos.Share.decode(data);
+                                        if (shareData.port === port) {
+                                                var d = new Date(shareData.timestamp);
+                                                console.log(d.toString() + ": " + JSON.stringify(shareData))
+                                        }
+                                });
+                        }
+                });
+                cursor.close();
+                txn.commit();
+                process.exit(0);
+        });
+});
