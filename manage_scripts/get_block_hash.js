298a73d8bdbdc4ccce36cb1b76ece0f62faaf9c2
MoneroOcean <support@moneroocean.stream>
2020-03-19 14:47:46 -0700

    Fixed message

--- manage_scripts/get_block_hash.js
+++ manage_scripts/get_block_hash.js
@@ -15,7 +15,7 @@ if (!argv.hash) {
 const hash = argv.hash;
 
 require("../init_mini.js").init(function() {
-  global.coinFuncs.getPortAnyBlockHeaderByHash(port, hash, true, function (err_header, body_header) {
+  global.coinFuncs.getPortAnyBlockHeaderByHash(port, hash, false, function (err_header, body_header) {
     console.log("err:"  + JSON.stringify(err_header));
     console.log("body:" + JSON.stringify(body_header));
     process.exit(0);
