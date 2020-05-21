a4543d0640608669f31a2ec9a103339e7ef85ccd
MoneroOcean <support@moneroocean.stream>
2020-05-21 09:10:43 -0700

    Fixed quotes

--- manage_scripts/news.sh
+++ manage_scripts/news.sh
@@ -6,4 +6,4 @@ body=$2
 if [ -z "$subject" ]; then echo "Set subject as first script paaameter"; exit 1; fi
 if [ -z "$body" ]; then echo "Set bosy as second script paaameter"; exit 1; fi
 
-node cache_set.js --key=news --value='{created: "'$(date +%s)'", subject: "'$subject'", body: "'$body'"}'
+node cache_set.js --key=news --value='{"created": "'$(date +%s)'", "subject": "'$subject'", "body": "'$body'"}'
