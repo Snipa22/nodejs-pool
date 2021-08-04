ad041014a4b6d2c87cfd2d3d4347615feb8c635c
MoneroOcean <support@moneroocean.stream>
2021-08-04 06:57:49 +0000

    Fixed to works with many blocks

--- manage_scripts/altblock_change_stage.js
+++ manage_scripts/altblock_change_stage.js
@@ -1,39 +1,37 @@
 "use strict";
 
-const argv = require('minimist')(process.argv.slice(2));
-
-if (!argv.hash) {
-	console.error("Please specify altblock hash");
-	process.exit(1);
-}
-const hash = argv.hash;
+const argv = require('minimist')(process.argv.slice(2), { '--': true });
 
 if (!argv.stage) {
-	console.error("Please specify new stage value");
-	process.exit(1);
+        console.error("Please specify new stage value");
+        process.exit(1);
 }
 const stage = argv.stage;
 
+let hashes = {};
+for (const h of argv['--']) {
+  hashes[h] = 1;
+}
+
 require("../init_mini.js").init(function() {
-	let txn = global.database.env.beginTxn();
+        let changed = 0;
+        let txn = global.database.env.beginTxn();
         let cursor = new global.database.lmdb.Cursor(txn, global.database.altblockDB);
         for (let found = cursor.goToFirst(); found; found = cursor.goToNext()) {
-        	cursor.getCurrentBinary(function(key, data){  // jshint ignore:line
-			let blockData = global.protos.AltBlock.decode(data);
-			if (blockData.hash === hash) {
-				console.log("Found altblock with " + blockData.hash + " hash");
-				blockData.pay_stage = stage;
-				console.log("Put \"" + blockData.pay_stage + "\" stage to block");
-				txn.putBinary(global.database.altblockDB, key, global.protos.AltBlock.encode(blockData));
-				txn.commit();
-				cursor.close();
-				console.log("Changed altblock");
-				process.exit(0);
-			}
-		});
+                cursor.getCurrentBinary(function(key, data){  // jshint ignore:line
+                        let blockData = global.protos.AltBlock.decode(data);
+                        if (blockData.hash in hashes) {
+                                console.log("Found altblock with " + blockData.hash + " hash");
+                                blockData.pay_stage = stage;
+                                console.log("Put \"" + blockData.pay_stage + "\" stage to block");
+                                txn.putBinary(global.database.altblockDB, key, global.protos.AltBlock.encode(blockData));
+                                console.log("Changed altblock");
+                                changed = 1;
+                        }
+                });
         }
         cursor.close();
         txn.commit();
-	console.log("Not found altblock with " + hash + " hash");
-	process.exit(1);
+        if (!changed) console.log("Not found altblocks with specified hashes");
+        process.exit(0);
 });
