d08d118161afaeaad4c0ff97ef097e0939a3fe1d
MoneroOcean <support@moneroocean.stream>
2022-02-11 16:01:26 +0000

    Updated monero daemon

--- deployment/upgrade_monero.bash
+++ deployment/upgrade_monero.bash
@@ -3,8 +3,8 @@ echo "This assumes that you have a standard nodejs-pool install, and will patch 
 sleep 15
 echo "Continuing install, this will prompt you for your password if you didn't enable passwordless sudo.  Please do not run me as root!"
 cd /usr/local/src/monero &&\
-sudo git checkout .  &&\
-sudo git checkout master &&\
+sudo git reset --hard origin/master &&\
+sudo git checkout &&\
 sudo git pull &&\
 sudo git checkout v0.17.3.0 &&\
 sudo git submodule foreach git pull &&\
